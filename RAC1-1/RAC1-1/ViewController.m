148ef689f1402759b1f0ceb6452e691ef3cb5a04
Your Name <LittleDream92>
2016-09-19 11:25:15 +0800

    RACSignal study 信号

--- RAC1-1/RAC1-1/ViewController.m
+++ RAC1-1/RAC1-1/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface ViewController ()
 
@@ -16,7 +17,49 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //***************** 我是华丽的分割线 *****************
+    //一、RACSignal信号
+    /*理论：
+     信号可以看做是传递信号的工具，当数据发生变化时，信号会发送改变的信息，以通知信号的订阅者执行方法。默认一个信号是冷信号，即使值改变也不会触发，只有订阅了这个信号才会变成热信号，值改变了才会触发。
+        核心：信号类
+        作用：只要数据改变，就会把数据包装成信号传递出去
+        触发条件：数据改变，信号发出
+        执行方法：通知信号的订阅者
+        实现思路：当一个信号被订阅，创建订阅者，并把nextBlock保存到订阅者里面；
+                创建信号会调用[RACDynamicSignal createSignal:didSubscribe];
+                发送信号时[subscriber sendNext:value];
+                订阅者会调用nextBlock
+     */
+    
+    //***************** 我是华丽的分割线 *****************
+    //1、创建信号
+    RACSignal *signal1 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        //3、发送信号
+        [subscriber sendNext:@"This is signal1"];
+        
+        [subscriber sendCompleted];
+        //4、取消信号：若信号要被取消，就必须返回取消信号的类
+        return [RACDisposable disposableWithBlock:^{
+            NSLog(@"cancel signal1");
+        }];
+    }];
+    
+    //2、订阅信号:返回值是一个取消订阅信号的类
+    RACDisposable *disposable1 = [signal1 subscribeNext:^(id x) {
+        //signal1发送信号后，就会调用这个nextBlock
+        NSLog(@"signal======%@", x);
+    } error:^(NSError *error) {
+        NSLog(@"signal1 error=====%@", error);
+    } completed:^{
+        NSLog(@"signal1 =====complete");
+    }];
+    //取消订阅
+    [disposable1 dispose];
+    
+    //疑问：1、发送完信号一定要取消信号吗？   不一定，也可以返回nil
+    //     2、返回取消信号和[disposable1 dispose];要同时都有吗？ 不造，谁知道答案可以告诉我！
 }
 
 - (void)didReceiveMemoryWarning {
