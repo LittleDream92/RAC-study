da12a76b3ce86a28bc87bb509fbf1a80dded5952
Your Name <LittleDream92>
2016-09-19 16:04:45 +0800

    lesson3:bind/Map/Combine/Filter……

---
+++ RAC1-3/RAC1-3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RAC1-3
+//
+//  Created by Meng Fan on 16/9/19.
+//  Copyright © 2016年 Meng Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RAC1-3/RAC1-3/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  RAC1-3
+//
+//  Created by Meng Fan on 16/9/19.
+//  Copyright © 2016年 Meng Fan. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
