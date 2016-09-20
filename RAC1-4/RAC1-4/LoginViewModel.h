ea4d50aaad71f311c0564687720d8c0e3a0d7994
Your Name <LittleDream92>
2016-09-20 11:16:59 +0800

    Login example MVVM

---
+++ RAC1-4/RAC1-4/LoginViewModel.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.h
+//  RAC1-4
+//
+//  Created by Meng Fan on 16/9/20.
+//  Copyright © 2016年 Meng Fan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoginViewModel : NSObject
+
+@end
---
+++ RAC1-4/RAC1-4/LoginViewModel.m
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.m
+//  RAC1-4
+//
+//  Created by Meng Fan on 16/9/20.
+//  Copyright © 2016年 Meng Fan. All rights reserved.
+//
+
+#import "LoginViewModel.h"
+
+@implementation LoginViewModel
+
+@end
